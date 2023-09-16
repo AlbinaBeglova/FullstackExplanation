9a098794054f5f112a342c5d0938686952c56edd
Albina Begl <lowflowhlow@gmail.com>
2023-09-16 22:03:35 +0300

    test commit

--- FullstackExplanation/lesson_01/lesson_01.js
+++ FullstackExplanation/lesson_01/lesson_01.js
@@ -1,1 +1,10 @@
-console.log("first lesson");
+const person = {
+    age: 25,
+    getAge: function () {
+      return this.age;
+    },
+  };
+  
+  console.log(person.getAge());
+  // Expected output: 25
+  
